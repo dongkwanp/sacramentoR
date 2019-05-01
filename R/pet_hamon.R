1ba5fe123a16e085c8c98be7f70dfdff7d7ed966
Don Park <dongkwanp@gmail.com>
2019-05-01 16:28:15 +0000

    Updated PET module to handle JDate calculation

--- R/pet_hamon.R
+++ R/pet_hamon.R
@@ -2,9 +2,9 @@
 #'
 #' Calculation of potential evapotranspiration based on the Hamon method
 #'
-#' @param Coeff Hamon PET Model Coefficient (Default: 1.2 from Lu (2005))
-#' @param JDate Julian Date (Time-series or numeric)
-#' @param Tavg Average Temperature (deg C) (Time-series or numeric)
+#' @param Coeff Hamon PET Model Coefficient (Default: 1 from Lu (2005))
+#' @param Date Date (Time-series vector or as a Date Object)
+#' @param Tavg Average Temperature (deg C) (Time-series vector or numeric)
 #' @param Latitude Latitude (Degrees)
 #' @param dayFUN Daylight hours function (Defaults to CBM Model)
 #'
@@ -12,7 +12,9 @@
 #' @details For details see Lu, Sun, McNulty, and Amatya (2005) and Haith and Shoemaker (1987)
 #' @export
 
-pet_hamon <- function(par, JDate, Tavg, Latitude, dayFUN = sacramentoR::daylight_CBM) {
+pet_hamon <- function(par, Date, Tavg, Latitude, dayFUN = sacramentoR::daylight_CBM) {
+
+  JDate <- format(as.Date(Date), '%j')
 
   daylight <- dayFUN(JDate, Latitude)
 
@@ -26,3 +28,20 @@ pet_hamon <- function(par, JDate, Tavg, Latitude, dayFUN = sacramentoR::daylight
 
   return(Hamon)
 }
+
+
+# Hamon Ranges ----
+pet_hamon.ranges <- function() {
+  list(
+    par <- c(0.8, 3) # Lu 2005 suggests default of 1.2 but other docs suggest 1
+  )
+}
+
+pet_hamon.defaults <- function() {
+  list(
+    par <- 1 # Default: 1.2 from Lu (2005)
+  )
+}
+
+
+
